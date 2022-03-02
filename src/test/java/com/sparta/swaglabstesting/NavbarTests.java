217109c342754ae2dd4e376cb4a160b0a850c6cd
Aiden Sykes <aidensykes1999@gmail.com>
2022-03-02 21:11:02 +0000

    Navbar Pages + Tests initialised

--- src/test/java/com/sparta/swaglabstesting/NavbarTests.java
+++ src/test/java/com/sparta/swaglabstesting/NavbarTests.java
@@ -1,4 +1,34 @@
 package com.sparta.swaglabstesting;
 
+import com.sparta.swaglabstesting.pom.LoginPage;
+import com.sparta.swaglabstesting.pom.POMUtils;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class NavbarTests {
+
+    private WebDriver webDriver;
+    private LoginPage login;
+
+    @Before
+    public void setup(){
+        POMUtils.setDriverLocation();
+        webDriver = POMUtils.setUpWebDriver();
+        login = new LoginPage(webDriver);
+    }
+
+
+    @After
+    public void tearDown() {
+        webDriver.quit();
+    }
+
+    @Given("I have opened the web browser")
+    public void openBrowser(){
+        webDriver = new ChromeDriver();
+    }
+
 }
---
+++ src/test/java/com/sparta/swaglabstesting/pom/NavbarPage.java
@@ -0,0 +1,10 @@
+package com.sparta.swaglabstesting.pom;
+
+public class NavbarPage {
+
+
+
+
+
+    
+}
