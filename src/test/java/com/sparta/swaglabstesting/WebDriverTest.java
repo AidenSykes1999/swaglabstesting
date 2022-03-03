ac398d8625a3c56dc521a9a944a870c5094ab8e1
Piotr Sulek <piotr.sulek@outlook.com>
2022-03-03 11:19:31 +0000

    Added a test runner class

--- src/test/java/com/sparta/swaglabstesting/WebDriverTest.java
+++ src/test/java/com/sparta/swaglabstesting/WebDriverTest.java
@@ -5,7 +5,6 @@ import com.sparta.swaglabstesting.webdrivers.WebDriverManagerFactory;
 import com.sparta.swaglabstesting.webdrivers.WebDriverType;
 import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 
